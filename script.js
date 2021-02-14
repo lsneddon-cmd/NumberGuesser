642096b2bbdb46f5c13de690c9ba19557aff07d0
lewissneddon <lewis.sneddon@icloud.com>
2021-02-14 22:13:18 +0000

    project complete

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ const generateTarget = () => {
   return Math.round(Math.random() * 9);
 }
 
-console.log(generateTarget());
+console.log('Generated Target: ' + generateTarget());
+
+const compareGuesses = (humanGuess, computerGuess, target) => {
+  return Math.abs(target - humanGuess) <= Math.abs(target - computerGuess);
+}
+
+console.log('Expected true: ' + compareGuesses(4, 8, 3));
+
+const updateScore = (player) => {
+  if (player === 'human') humanScore++;
+  if (player === 'computer') computerScore++;
+}
+
+const advanceRound = () => {
+  currentRoundNumber++;
+}
